8f8f7b4558b30296e7f70d3b72b53d2fd31b09dc
Arka <arkaseth@gmail.com>
2020-09-15 03:34:47 +0530

    Updates

---
+++ precache-manifest.5b34b4a181a221d49d3090258fd58af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ab2c6f1c782b02f21e11a0b497ee8f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "99413dfbe131a2f94d6d",
+    "url": "/monsters-rolodex/static/css/main.5b78e286.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99413dfbe131a2f94d6d",
+    "url": "/monsters-rolodex/static/js/main.6b2dc4d8.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
